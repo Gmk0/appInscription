6f615e4264ae7a65f3638a6db8f1b4e7a8c7b8fc
Gmk0 <mkgeorges61@gmail.com>
2022-07-30 14:18:37 +0200

    debut gmk

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,27 +15,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-return view('Client.accueil');
+    return view('Client.accueil');
 });
 
-Route::prefix('student')->group( function (){
-    Route::view('/','Client.accueil')->name('accueil.student');
-    Route::view('/registration','Client.InscriptionForms')->name('inscription');
-    Route::view('/about','Client.about')->name('about');
-    Route::view('/registration_complet','Client.registrationComplet')->name('registration_complet');
+Route::prefix('student')->group(function () {
+    Route::view('/', 'Client.accueil')->name('accueil.student');
+    Route::view('/registration', 'Client.InscriptionForms')->name('inscription');
+    Route::view('/about', 'Client.about')->name('about');
+    Route::view('/registration_complet', 'Client.registrationComplet')->name('registration_complet');
     Route::get('/formulaire/{matricule}', [\App\Http\Controllers\reporting::class, 'formulaire'])->where('matricule', '(.*)')->name('formulaire');
     Route::get('/profil', \App\Http\Livewire\ProfilEtudiantUpdate::class)->name('profilEtudiant');
     Route::get('/checkout/{matricule}', \App\Http\Livewire\CheckoutInscription::class)->where('matricule', '(.*)')->name('checkout');
 });
 
-route::prefix('adminUsakin')->group( function(){
+route::prefix('adminUsakin')->group(function () {
     Auth::routes();
 
-    route::view('/','Admin.dashboard')->name('Admin.dashboard')->middleware('auth');
+    route::view('/', 'Admin.dashboard')->name('Admin.dashboard')->middleware('auth');
     Route::get('/users', \App\Http\Livewire\Users::class)->name('users.admin')->middleware('auth');
     Route::get('/etudiant', \App\Http\Livewire\EtudiantInscris::class)->name('users.etudiant')->middleware('auth');
     Route::get('/All-etudiant', \App\Http\Livewire\AllStudent::class)->name('users.allEtdiant')->middleware('auth');
-    
+
     Route::get('/gestion_faculte', \App\Http\Livewire\FacultePromotion::class)->name('gestion_faculte.admin')->middleware('auth');
     Route::get('/etudiant/{id}', \App\Http\Livewire\FindEtudiantAdmin::class)->name('FindEtudiant')->middleware('auth');
 });
